ebaa21422f8ac0bb5e5a01725e86fe138da524b5
Dragos Daian <daian.dragos@yahoo.com>
2025-11-22 23:25:56 +0100

    lint

--- lib/game_server/repo.ex
+++ lib/game_server/repo.ex
@@ -1,10 +1,10 @@
 defmodule GameServer.Repo do
   @adapter if System.get_env("DATABASE_URL") ||
-             System.get_env("POSTGRES_HOST") ||
-             System.get_env("POSTGRES_USER") ||
-             System.get_env("POSTGRES_DB"),
-           do: Ecto.Adapters.Postgres,
-           else: Ecto.Adapters.SQLite3
+                System.get_env("POSTGRES_HOST") ||
+                System.get_env("POSTGRES_USER") ||
+                System.get_env("POSTGRES_DB"),
+              do: Ecto.Adapters.Postgres,
+              else: Ecto.Adapters.SQLite3
 
   use Ecto.Repo,
     otp_app: :game_server,
