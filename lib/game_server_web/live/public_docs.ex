944e77c0d9d0f850f96dfa994a550d8f5feec96e
Dragos Daian <daian.dragos@yahoo.com>
2025-11-23 23:48:18 +0100

    update

--- lib/game_server_web/live/public_docs.ex
+++ lib/game_server_web/live/public_docs.ex
@@ -19,7 +19,7 @@ defmodule GameServerWeb.PublicDocs do
               <svg class="w-8 h-8" viewBox="0 0 24 24" fill="currentColor">
                 <path d="M17.05 20.28c-.98.95-2.05.8-3.08.35-1.09-.46-2.09-.48-3.24 0-1.44.62-2.2.44-3.06-.35C2.79 15.25 3.51 7.59 9.05 7.31c1.35.07 2.29.74 3.08.8 1.18-.24 2.31-.93 3.57-.84 1.51.12 2.65.72 3.4 1.8-3.12 1.87-2.38 5.98.48 7.13-.57 1.5-1.31 2.99-2.54 4.09l.01-.01zM12.03 7.25c-.15-2.23 1.66-4.07 3.74-4.25.29 2.58-2.34 4.5-3.74 4.25z" />
               </svg>
-              Apple Sign In Setup Guide
+              Apple Sign In Setup
               <button
                 type="button"
                 data-action="toggle-card"
@@ -365,7 +365,7 @@ defmodule GameServerWeb.PublicDocs do
               <svg class="w-8 h-8 text-indigo-600" fill="currentColor" viewBox="0 0 24 24">
                 <path d="M20.317 4.492c-1.53-.69-3.17-1.2-4.885-1.49a.075.075 0 0 0-.079.036c-.21.369-.444.85-.608 1.23a18.566 18.566 0 0 0-5.487 0 12.36 12.36 0 0 0-.617-1.23A.077.077 0 0 0 8.562 3c-1.714.29-3.354.8-4.885 1.491a.07.07 0 0 0-.032.027C.533 9.093-.32 13.555.099 17.961a.08.08 0 0 0 .031.055 20.03 20.03 0 0 0 5.993 2.98.078.078 0 0 0 .084-.026 13.83 13.83 0 0 0 1.226-1.963.074.074 0 0 0-.041-.104 13.201 13.201 0 0 1-1.872-.878.075.075 0 0 1-.008-.125c.126-.093.252-.19.372-.287a.075.075 0 0 1 .078-.01c3.927 1.764 8.18 1.764 12.061 0a.075.075 0 0 1 .079.009c.12.098.245.195.372.288a.075.075 0 0 1-.006.125c-.598.344-1.22.635-1.873.877a.075.075 0 0 0-.041.105c.36.687.772 1.341 1.225 1.962a.077.077 0 0 0 .084.028 19.963 19.963 0 0 0 6.002-2.981.076.076 0 0 0 .032-.054c.5-5.094-.838-9.52-3.549-13.442a.06.06 0 0 0-.031-.028zM8.02 15.278c-1.182 0-2.157-1.069-2.157-2.38 0-1.312.956-2.38 2.157-2.38 1.21 0 2.176 1.077 2.157 2.38 0 1.312-.956 2.38-2.157 2.38zm7.975 0c-1.183 0-2.157-1.069-2.157-2.38 0-1.312.955-2.38 2.157-2.38 1.21 0 2.176 1.077 2.157 2.38 0 1.312-.946 2.38-2.157 2.38z" />
               </svg>
-              Discord OAuth Setup Guide
+              Discord OAuth Setup
               <button
                 type="button"
                 data-action="toggle-card"
@@ -579,7 +579,7 @@ defmodule GameServerWeb.PublicDocs do
                   fill="#EA4335"
                 />
               </svg>
-              Google OAuth Setup Guide
+              Google OAuth Setup
               <button
                 type="button"
                 data-action="toggle-card"
@@ -790,7 +790,7 @@ defmodule GameServerWeb.PublicDocs do
               <svg class="w-8 h-8 text-blue-600" fill="currentColor" viewBox="0 0 24 24">
                 <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z" />
               </svg>
-              Facebook OAuth Setup Guide
+              Facebook OAuth Setup
               <button
                 type="button"
                 data-action="toggle-card"
@@ -1080,7 +1080,7 @@ defmodule GameServerWeb.PublicDocs do
                   d="M3 8l7.89 4.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
                 />
               </svg>
-              Email Configuration Guide
+              Email Setup
               <button
                 type="button"
                 data-action="toggle-card"
@@ -1191,7 +1191,7 @@ defmodule GameServerWeb.PublicDocs do
                 <path d="M13.632 2.286c.176.08.293.245.293.438v1.34c1.725.447 3.013 1.816 3.013 3.482 0 1.946-1.58 3.527-3.526 3.527-.948 0-1.814-.386-2.446-1.01-.632.624-1.498 1.01-2.446 1.01C6.58 11.073 5 9.492 5 7.546c0-1.666 1.288-3.035 3.013-3.482V2.724c0-.193.117-.358.293-.438L9.6 1.2c.176-.08.39-.08.566 0l1.466.666z" />
                 <path d="M12 14.5c-1.38 0-2.5 1.12-2.5 2.5v7c0 1.38 1.12 2.5 2.5 2.5s2.5-1.12 2.5-2.5v-7c0-1.38-1.12-2.5-2.5-2.5z" />
               </svg>
-              Sentry Error Monitoring Setup
+              Sentry Setup
               <button
                 type="button"
                 data-action="toggle-card"
@@ -1392,7 +1392,7 @@ defmodule GameServerWeb.PublicDocs do
                   d="M4 7v10c0 2.21 3.582 4 8 4s8-1.79 8-4V7M4 7c0 2.21 3.582 4 8 4s8-1.79 8-4M4 7c0-2.21 3.582-4 8-4s8 1.79 8 4"
                 />
               </svg>
-              PostgreSQL Configuration (Alternative to SQLite)
+              PostgreSQL Setup
               <button
                 type="button"
                 data-action="toggle-card"
