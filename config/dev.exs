f15994ba0337cbd2a24e87e6fecdf1231dbc66bd
Dragos Daian <daian.dragos@yahoo.com>
2025-11-23 09:48:37 +0100

    update

--- config/dev.exs
+++ config/dev.exs
@@ -31,9 +31,9 @@ end
 # watchers to your application. For example, we can use it
 # to bundle .js and .css sources.
 config :game_server, GameServerWeb.Endpoint,
-  # Binding to loopback ipv4 address prevents access from other machines.
-  # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}, port: String.to_integer(System.get_env("PORT") || "4000")],
+  # Binding to all interfaces to allow access from Docker host and other machines.
+  # In Docker containers, bind to 0.0.0.0 to accept external connections.
+  http: [ip: {0, 0, 0, 0}, port: String.to_integer(System.get_env("PORT") || "4000")],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
---
+++ migrate.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Run Ecto migrations against PostgreSQL database
+# Usage: ./migrate.sh
+
+export POSTGRES_HOST=localhost
+export POSTGRES_USER=postgres
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_DB=game_server_dev
+
+mix ecto.migrate
