4aa8ae6468e2b58c5936d3c0e6d402507fd20824
Dragos Daian <daian.dragos@yahoo.com>
2025-12-24 22:29:16 +0200

    upd

--- sdk_tools/mix.exs
+++ sdk_tools/mix.exs
@@ -1,7 +1,7 @@
 defmodule GameServerPluginTools.MixProject do
   use Mix.Project
 
-  @version "1.0.4"
+  @version "1.0.6"
   @source_url "https://github.com/appsinacup/game_server"
 
   def project do
