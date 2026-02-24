cab4ab289c6978285b126ac98793955f23b2da82
Dragos Daian <daian.dragos@yahoo.com>
2026-02-24 17:18:43 +0100

    add fix for notification

--- apps/game_server_core/priv/repo/migrations/20260222123000_add_unique_notification_sender_recipient_title.exs
+++ apps/game_server_core/priv/repo/migrations/20260222123000_add_unique_notification_sender_recipient_title.exs
@@ -1,7 +1,36 @@
 defmodule GameServer.Repo.Migrations.AddUniqueNotificationSenderRecipientTitle do
   use Ecto.Migration
 
-  def change do
-    create unique_index(:notifications, [:sender_id, :recipient_id, :title])
+  def up do
+    execute("""
+    DELETE FROM notifications
+    WHERE id IN (
+      SELECT id
+      FROM (
+        SELECT
+          id,
+          ROW_NUMBER() OVER (
+            PARTITION BY sender_id, recipient_id, title
+            ORDER BY inserted_at DESC, id DESC
+          ) AS row_num
+        FROM notifications
+      ) ranked
+      WHERE ranked.row_num > 1
+    )
+    """)
+
+    create_if_not_exists(
+      unique_index(:notifications, [:sender_id, :recipient_id, :title],
+        name: :notifications_sender_id_recipient_id_title_index
+      )
+    )
+  end
+
+  def down do
+    drop_if_exists(
+      index(:notifications, [:sender_id, :recipient_id, :title],
+        name: :notifications_sender_id_recipient_id_title_index
+      )
+    )
   end
 end
