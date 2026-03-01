1bbecf4307140d315c4806d9e5632235492b6d48
Dragos Daian <daian.dragos@yahoo.com>
2026-03-01 13:08:24 +0100

    lint again

--- apps/game_server_web/lib/game_server_web/components/layouts.ex
+++ apps/game_server_web/lib/game_server_web/components/layouts.ex
@@ -278,7 +278,7 @@ defmodule GameServerWeb.Layouts do
             <.theme_toggle />
           </li>
         </ul>
-
+        
     <!-- Mobile Navigation -->
         <div class="lg:hidden">
           <div class="dropdown dropdown-end">
