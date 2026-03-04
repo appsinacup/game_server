31994fa4c6629ce319a94dbfbfe6c0efe8d7515d
Dragos Daian <daian.dragos@yahoo.com>
2026-03-04 21:01:20 +0100

    upd

--- apps/game_server_web/priv/static/game/index.service.worker.js
+++ apps/game_server_web/priv/static/game/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1772635371|53750635';
+const CACHE_VERSION = '1772654315|326898690';
 /** @type {string} */
 const CACHE_PREFIX = 'Gamend Starter-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
