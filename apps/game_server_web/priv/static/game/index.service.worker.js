f33a98d7b88b177081367f6f06d9b54a502f15da
Dragos Daian <daian.dragos@yahoo.com>
2026-03-04 22:15:40 +0100

    revert texture import change

--- apps/game_server_web/priv/static/game/index.service.worker.js
+++ apps/game_server_web/priv/static/game/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1772656108|2120181225';
+const CACHE_VERSION = '1772658911|37080866';
 /** @type {string} */
 const CACHE_PREFIX = 'Gamend Starter-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
