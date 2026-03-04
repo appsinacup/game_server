aee9926e822b854d4346cc98381956f6c7bc696c
Dragos Daian <daian.dragos@yahoo.com>
2026-03-04 15:40:39 +0100

    re-order stuff

--- apps/game_server_web/lib/game_server_web/live/public_docs.ex
+++ apps/game_server_web/lib/game_server_web/live/public_docs.ex
@@ -18,27 +18,39 @@ defmodule GameServerWeb.PublicDocs do
           </:subtitle>
         </.header>
 
-        {GameServerWeb.PublicDocsTemplates.js_sdk(assigns)}
-        {GameServerWeb.PublicDocsTemplates.godot_sdk(assigns)}
-        {GameServerWeb.PublicDocsTemplates.server_scripting(assigns)}
-        {GameServerWeb.PublicDocsTemplates.leaderboards(assigns)}
-        {GameServerWeb.PublicDocsTemplates.theme(assigns)}
+        <%!-- Overview & Fundamentals --%>
         {GameServerWeb.PublicDocsTemplates.architecture(assigns)}
-        {GameServerWeb.PublicDocsTemplates.custom_host(assigns)}
-        {GameServerWeb.PublicDocsTemplates.authentication(assigns)}
-        {GameServerWeb.PublicDocsTemplates.realtime(assigns)}
         {GameServerWeb.PublicDocsTemplates.data_schema(assigns)}
+        {GameServerWeb.PublicDocsTemplates.authentication(assigns)}
+
+        <%!-- SDK Integration --%>
+        {GameServerWeb.PublicDocsTemplates.godot_sdk(assigns)}
+        {GameServerWeb.PublicDocsTemplates.js_sdk(assigns)}
+
+        <%!-- Core Features --%>
+        {GameServerWeb.PublicDocsTemplates.realtime(assigns)}
+        {GameServerWeb.PublicDocsTemplates.leaderboards(assigns)}
+        {GameServerWeb.PublicDocsTemplates.chat(assigns)}
+        {GameServerWeb.PublicDocsTemplates.server_scripting(assigns)}
+        {GameServerWeb.PublicDocsTemplates.theme(assigns)}
+
+        <%!-- Extending & Customizing --%>
+        {GameServerWeb.PublicDocsTemplates.custom_host(assigns)}
+
+        <%!-- OAuth Provider Setup --%>
         {GameServerWeb.PublicDocsTemplates.apple_sign_in(assigns)}
-        {GameServerWeb.PublicDocsTemplates.well_known(assigns)}
         {GameServerWeb.PublicDocsTemplates.steam_openid(assigns)}
         {GameServerWeb.PublicDocsTemplates.discord_oauth(assigns)}
         {GameServerWeb.PublicDocsTemplates.google_oauth(assigns)}
         {GameServerWeb.PublicDocsTemplates.facebook_oauth(assigns)}
+
+        <%!-- Infrastructure & Operations --%>
         {GameServerWeb.PublicDocsTemplates.email_setup(assigns)}
         {GameServerWeb.PublicDocsTemplates.sentry_setup(assigns)}
         {GameServerWeb.PublicDocsTemplates.cache_setup(assigns)}
         {GameServerWeb.PublicDocsTemplates.scaling(assigns)}
         {GameServerWeb.PublicDocsTemplates.postgresql_setup(assigns)}
+        {GameServerWeb.PublicDocsTemplates.well_known(assigns)}
       </div>
     </Layouts.app>
     """
