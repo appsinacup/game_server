616cb584d1671098920256cbb7c0e5599e18d7b8
Dragos Daian <daian.dragos@yahoo.com>
2026-03-07 22:38:53 +0100

    serialize "" as description if null for groups

--- apps/game_server_web/lib/game_server_web/controllers/api/v1/group_controller.ex
+++ apps/game_server_web/lib/game_server_web/controllers/api/v1/group_controller.ex
@@ -1286,7 +1286,7 @@ defmodule GameServerWeb.Api.V1.GroupController do
     %{
       id: group.id,
       title: group.title,
-      description: group.description,
+      description: group.description || "",
       type: group.type,
       max_members: group.max_members,
       metadata: group.metadata || %{},
