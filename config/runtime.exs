e5b7968ec0de51b8082cb5bf342efd27c0f83bad
Dragos Daian <daian.dragos@yahoo.com>
2026-04-13 09:25:26 +0200

    lint

--- config/runtime.exs
+++ config/runtime.exs
@@ -458,7 +458,7 @@ if config_env() == :prod do
         port: https_port,
         cipher_suite: :strong,
         certfile: ssl_certfile,
-        keyfile: ssl_keyfile,
+        keyfile: ssl_keyfile
         # Suppress noisy TLS handshake notices from bots/scanners
         # probing with old TLS versions or unsupported cipher suites.
       ]
